022d83a8f6d0e97a80a537484158c62ff8f1a8b4
Doug <higoshi.douglas@gmail.com>
2024-08-07 22:33:59 -0300

    Creating user service and annotating entities with entity and table annotations

--- src/main/java/br/com/doug/library/entity/Return.java
+++ src/main/java/br/com/doug/library/entity/Return.java
@@ -2,7 +2,17 @@ package br.com.doug.library.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "returns")
 public class Return {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long user;
     private Long book;
---
+++ src/main/java/br/com/doug/library/service/UserService.java
@@ -0,0 +1,34 @@
+package br.com.doug.library.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import br.com.doug.library.entity.User;
+import br.com.doug.library.repository.UserRepository;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> get(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
