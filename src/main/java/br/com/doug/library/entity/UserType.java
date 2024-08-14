7eb08892111def20b84a5d3e7647624af00ba5e9
Doug <higoshi.douglas@gmail.com>
2024-08-14 19:10:45 -0300

    Adding many to one relationship between user types and users

--- src/main/java/br/com/doug/library/entity/UserType.java
+++ src/main/java/br/com/doug/library/entity/UserType.java
@@ -1,18 +1,23 @@
 package br.com.doug.library.entity;
 
+import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "userTypes")
+@Table(name = "user_types")
 public class UserType {
     @Id
     private Long id;
     private String userType;
 
+    @OneToMany(mappedBy = "userType")
+    private List<User> user;
+
     public UserType() {
-        super();
+
     }
 
     public UserType(Long id, String userType) {
@@ -34,6 +39,14 @@ public class UserType {
 
     public void setUserType(String userType) {
         this.userType = userType;
+    }
+
+    public List<User> getUser() {
+        return user;
+    }
+
+    public void setUser(List<User> user) {
+        this.user = user;
     }
 
 }
